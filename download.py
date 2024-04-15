28d674f08272f29545622f7ef61d4492aa17c49b
breadbread1984 <breadbread1984@163.com>
2024-04-15 13:50:57 +0800

    update

--- download.py
+++ download.py
@@ -6,5 +6,5 @@ from paperscraper.pdf import save_pdf_from_dump
 query = ['CNT dispersant', 'carbon nanotube dispersion']
 for key in QUERY_FN_DICT.keys():
   QUERY_FN_DICT[key](query, output_filepath = '%s_result.jsonl' % key)
-  save_pdf_from_dump('%s_result.json' % key, pdf_path = key, key_to_save = 'doi')
+  save_pdf_from_dump('%s_result.jsonl' % key, pdf_path = key, key_to_save = 'doi')
 
